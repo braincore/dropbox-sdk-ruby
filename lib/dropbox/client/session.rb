80d8c83c4e11e62635973c0925ea8a9f33a52c55
Elmer Le <elmer@dropbox.com>
2014-07-24 19:19:52 -0700

    Fixed minor bug.

--- lib/dropbox/client/session.rb
+++ lib/dropbox/client/session.rb
@@ -3,32 +3,32 @@ module Dropbox
 
     class Session
 
-      def initialize(oauth2_access_token, client_identitifier, locale)
-        @oauth2_token = oauth2_access_token
-        @client_identitifier = client_identitifier
+      def initialize(oauth2_access_token, client_identifier, locale)
+        @oauth2_access_token = oauth2_access_token
+        @client_identifier = client_identifier
         @locale = locale
       end
 
       def do_get(host, path, params = {}, headers = {})  # :nodoc:
         sign_and_set_locale(params, headers)
-        Dropbox::API::HTTP.do_http_request(Net::HTTP::Get, host, path, params, headers)
+        Dropbox::API::HTTP.do_http_request(Net::HTTP::Get, host, path, @client_identifier, params, headers)
       end
 
       def do_post(host, path, params = {}, headers = {})  # :nodoc:
         sign_and_set_locale(params, headers)
-        Dropbox::API::HTTP.do_http_request(Net::HTTP::Post, host, path, nil, headers, params)
+        Dropbox::API::HTTP.do_http_request(Net::HTTP::Post, host, path, @client_identifier, nil, headers, params)
       end
 
       def do_put(host, path, params = {}, headers = {}, body = nil)  # :nodoc:
         sign_and_set_locale(params, headers)
-        Dropbox::API::HTTP.do_http_request(Net::HTTP::Put, host, path, params, headers, body)
+        Dropbox::API::HTTP.do_http_request(Net::HTTP::Put, host, path, @client_identifier, params, headers, body)
       end
 
       private
 
       def sign_and_set_locale(params, headers)
         params['locale'] = @locale
-        headers['Authorization'] = "Bearer #{ @access_token }"
+        headers['Authorization'] = "Bearer #{ @oauth2_access_token }"
       end
 
     end
